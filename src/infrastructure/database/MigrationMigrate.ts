77ae88fafe263e82bfe6ea2cff8b4909fe455add
diego breeg <diegobreeg@gmail.com>
2025-02-20 18:00:32 -0300

    [IMPLEMENTATION] Implements migration system

--- src/infrastructure/database/MigrationMigrate.ts
+++ src/infrastructure/database/MigrationMigrate.ts
@@ -1,21 +1,82 @@
-import * as fs              from 'fs';
-import * as path            from 'path';
-import { MySQLConnection }  from './MySQLConnection';
+import * as fs                      from 'fs';
+import * as path                    from 'path';
+import { MySQLConnection }          from './MySQLConnection';
+import { RowDataPacket } from 'mysql2';
 
 export class MigrationMigrate {
-    static readonly #database                          = MySQLConnection.getInstance();
-    static readonly #migrationDir                      = path.join(__dirname, "migrations");
+    static readonly #database                           = MySQLConnection.getInstance();
+    static readonly #migrationsDir                      = path.join(__dirname, "migrations");
+    static readonly #arguments                          = process.argv[2];
+    static readonly #databaseName                       = process.env.MYSQL_DATABASE;
 
-    static execute() {
-        const argument = process.argv[2];
-        const migrations = fs.readdirSync(MigrationMigrate.#migrationDir);
-        console.log(migrations);
-        MigrationMigrate.loadMigrations()
+    static async execute() {
+        try {
+            await MigrationMigrate.#database.ensureDatabase();
+            await MigrationMigrate.ensureMigrationTable();
+            const migrations = await MigrationMigrate.loadMigrations();
+            const alreadyMigrated = await MigrationMigrate.loadAlreadyMigrated();
+            const migrationsToApply = MigrationMigrate.getMigrationsToApply(migrations, alreadyMigrated);
+
+            for (const migration of migrationsToApply) {
+                await MigrationMigrate.#database.query(migration.sql);
+                await MigrationMigrate.registerMigration(migration.version);
+                console.log("\n");
+                console.log("\x1b[32m%s\x1b[0m", `Version: ${migration.version} successfully migrated!`);
+                console.log("\x1b[32m%s\x1b[0m", `${migration.sql}`);
+                console.log("\n");
+            }
+        } catch (error) {
+            console.error("\x1b[31m%s\x1b[0m", "Error during migration:", error);
+        } finally {
+            await MigrationMigrate.#database.close();
+        }
     }
 
-    private static loadMigrations() {
+    private static async ensureMigrationTable() {
+        const sql = `
+        CREATE TABLE IF NOT EXISTS migrations (
+            version varchar(255) NOT NULL,
+            created_at DATETIME NOT NULL,
+            deleted_at DATETIME,
+            PRIMARY KEY (version)
+        );`;
 
+        await MigrationMigrate.#database.query(sql);
+        console.log("\x1b[32m%s\x1b[0m", `Table ${MigrationMigrate.#databaseName} ensured!`);
+        return;
+    }
+
+    private static async loadMigrations() {
+        const files = fs.readdirSync(MigrationMigrate.#migrationsDir).sort();
+        const migrations = await Promise.all(
+            files.map(file => import(path.join(MigrationMigrate.#migrationsDir, file)))
+        );
+
+        return migrations.map(migration => {
+            const version = Object.keys(migration)[0];
+            const sql = migration[version].up();
+            return {version: version.replace("Migration", ""), sql};
+        });
+    }
+
+    private static async loadAlreadyMigrated(): Promise<RowDataPacket[]> {
+        return await MigrationMigrate.#database.query("SELECT * FROM migrations WHERE deleted_at IS NULL;");
+    }
+
+    private static getMigrationsToApply(migrations: any[], alreadyMigrated: any[]) {
+        return migrations.filter(migration =>
+            !alreadyMigrated.some(already => already.version === migration.version)
+        );
+    }
+
+    private static async registerMigration(version: string) {
+        const sql = `
+            INSERT INTO migrations (version, created_at)
+            VALUES (${version}, NOW())
+        ;`;
+
+        await MigrationMigrate.#database.query(sql);
     }
 }
 
-MigrationMigrate.execute();
+MigrationMigrate.execute()
--- src/infrastructure/database/MySQLConnection.ts
+++ src/infrastructure/database/MySQLConnection.ts
@@ -1,5 +1,5 @@
-import mysql, { Pool, PoolOptions, RowDataPacket, ResultSetHeader }     from 'mysql2/promise';
-import { mySQLConnectionConfig }                                        from './MySQLConnectionConfig';
+import mysql, { Pool, PoolOptions, RowDataPacket, ResultSetHeader, Connection }     from 'mysql2/promise';
+import { mySQLConnectionConfig }                                                    from './MySQLConnectionConfig';
 
 export class MySQLConnection {
     private static instance     : MySQLConnection;
@@ -21,14 +21,37 @@ export class MySQLConnection {
         return this.pool;
     }
 
+    public async getConnection(): Promise<Connection> {
+        return await this.pool.getConnection();
+    }
+
     public async query<T extends RowDataPacket[] | RowDataPacket[][] | ResultSetHeader>(
         sql         : string,
         params      ?: any[]
-    ): Promise<[T, any]> {
-        return await this.pool.query<T>(sql, params);
+    ): Promise<T> {
+        const connection = await this.pool.getConnection();
+        const [rows] = await connection.query<T>(sql, params);
+        connection.release();
+        return rows;
     }
 
     public async close(): Promise<void> {
         await this.pool.end();
+    }
+
+    public async ensureDatabase(): Promise<void> {
+        const connection: Connection = await mysql.createConnection({
+            host        : mySQLConnectionConfig.host,
+            user        : mySQLConnectionConfig.user,
+            password    : mySQLConnectionConfig.password
+        });
+
+        const sql = `CREATE DATABASE IF NOT EXISTS \`${mySQLConnectionConfig.database}\``;
+        await connection.query<RowDataPacket[]>(sql);
+
+       console.log("\x1b[32m%s\x1b[0m", `Database ${mySQLConnectionConfig.database} ensured!`);
+
+       await connection.end();
+       return;
     }
 }
